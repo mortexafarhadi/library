ec4c85d6095999e242409b99aae032a9c36922d3
Mortexa Farhadi <mortexafarhadi@gmail.com>
2023-08-19 11:13:45 -0700

    add is_active to author - remove is_active from publisher

--- author/models.py
+++ author/models.py
@@ -11,6 +11,7 @@ class Author(models.Model):
     image = models.ImageField(upload_to='images/Author', null=True, blank=True)
     slug = models.SlugField(null=True, blank=True)
     join_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
