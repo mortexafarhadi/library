0387fd5f65b1c05741c31c300a911d88d5742956
Mortexa Farhadi <mortexafarhadi@gmail.com>
2023-08-29 08:10:11 -0700

    add discont field to book model - rename title to name for publisher model - add image,about field to publisher model - complete publisher detail template

--- book/models.py
+++ book/models.py
@@ -8,14 +8,25 @@ from publisher.models import Publisher
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    publisher = models.ForeignKey(Publisher,on_delete=models.CASCADE,null=True,blank=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     summary = models.TextField(blank=True, null=True)
+    discount = models.PositiveSmallIntegerField(default=0)
     page_count = models.PositiveSmallIntegerField(default=0)
     created_at = models.DateField(auto_now=True)
-    is_active = models.BooleanField(default=True)
     image = models.ImageField(upload_to='images/Book', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
+    def price_finally(self):
+        price = self.price
+        discount = self.discount
+        if discount < 1:
+            return price
+        else:
+            x = (price * discount) / 100
+            result_price = price - x
+            return result_price
--- publisher/urls.py
+++ publisher/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from publisher import views
 
 urlpatterns = [
-    path('all/', views.all_publisher),
+    path('all/', views.all_publishers, name='all_publishers'),
+    path('<slug:slug>/', views.publisher_details, name='publisher-details')
 ]
