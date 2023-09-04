2281944bfacb6707e623e5b6c394c8314dd1d9f4
mahdiye <mahdiye.saedi11@gmail.com>
2023-09-04 13:04:33 +0330

    create def index, link url to views

--- library/urls.py
+++ library/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('',include('site_setting.urls')),
     path('admin/', admin.site.urls),
     path('author/', include('author.urls')),
     path('publisher/', include('publisher.urls')),
